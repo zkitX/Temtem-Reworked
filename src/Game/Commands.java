455b797e084c3e0569986c190ad8070d392653f6
zkitX <ekosater@gmail.com>
2020-02-04 23:17:13 -0500

    More command definitions

--- src/Game/Commands.java
+++ src/Game/Commands.java
@@ -5,8 +5,48 @@ public class Commands {
 
     public final static String getPlayerData = "temtemWelfare.GetPlayerData";
 
-    public final static String swapTem = "pcControl.Swap";
-    public final static String getBox = "pcControl.GetBox";
-    public final static String releaseTem = "pcControl.Release";
-    public final static String
+
+    // PC Commands
+    public static String swapTem = "pcControl.Swap";
+    public static String getBox = "pcControl.GetBox";
+    public static String releaseTem = "pcControl.Release";
+    public static String releaseMultipleTem = "pcControl.ReleaseMID";
+    public static String BattleBoxSawp = "pcControl.BBSwap";
+    public static String BattleBoxSet = "pcControl.BBSet";
+    public static String BattleBoxUnset = "pcControl.BBUnset";
+    public static String PCUnknown2 = "pcControl.Bsa";
+    public static String getBattleBoxes = "pcControl.GetBattleBoxes";
+
+    // Inventory Commands
+    public static String dmgmcnghrnd = "inventory.GotWorldItem";
+    public static String pmgikdopedf = "inventory.GotItemFromNPC";
+    public static String ieiimlmknnk = "inventory.GotMonsterFromNPC";
+    public static String elllndiqgpq = "inventory.GiveItemToNPC";
+    public static String ngeonciomrc = "inventory.GiveMonsterToNPC";
+    public static String kdgceqflonl = "inventory.TradeMonsterWithNPC";
+    public static String ilfejhlplng = "inventory.BuyItemsOnShop";
+    public static String plcqcjnnqmi = "inventory.SellItemsOnShop";
+    public static String jomjccngdrh = "inventory.SellCosmeticOnShop";
+    public static String icfrifinqfh = "inventory.BuyCosmeticOnShop";
+    public static String lfieidpgkin = "inventory.UseMedicineItem";
+    public static String pqlfhmrppnn = "inventory.UseCourseItem";
+    public static String fpqqgghdkhd = "inventory.UseItem";
+    public static String idpqfmflmhg = "inventory.UseFlask";
+    public static String nlrpcicqgee = "inventory.EquipHeldItem";
+    public static String jifdjeqorch = "inventory.UnequipHeldItem";
+    public static String chdnlijpncj = "inventory.LoadCosmeticsInventory";
+    public static String jolhpiidhhe = "inventory.UpdPlayerCosmetics";
+    public static String onjcgqejllp = "inventory.RevertCosmeticColor";
+    public static String hhgkggfdqlq = "inventory.ChangeCosmeticColor";
+
+    // Competitive Commands
+    public static String pickBanHover = "pickAndBan.SlotHovered";
+    public static String pickBanSelected = "pickAndBan.SlotSelected";
+
+    // Player Actions
+    public static String Idle = "playerAction.Idle";
+    public static String Spawn = "playerAction.Spawn";
+    public static String Jump = "playerAction.Jmp";
+
+
 }
